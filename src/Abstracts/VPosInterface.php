bef831197a3488f8ce023889eec691ddaa777226
Seçgin ŞANLI <secgin@Secgin-MacBook-Pro.local>
2023-09-09 20:11:53 +0300

    VPosInterface ek açıklama metod tanımları yapıldı

--- src/Abstracts/VPosInterface.php
+++ src/Abstracts/VPosInterface.php
@@ -2,6 +2,15 @@
 
 namespace YG\VakifBankVPos\Abstracts;
 
+use YG\VakifBankVPos\Abstracts\Authentication\EnrollmentControlRequest;
+use YG\VakifBankVPos\Abstracts\Authentication\EnrollmentControlResponse;
+use YG\VakifBankVPos\Abstracts\Sale\SaleRequest;
+use YG\VakifBankVPos\Abstracts\Sale\SaleResponse;
+
+/**
+ * @method EnrollmentControlResponse enrollmentControl(EnrollmentControlRequest $request)
+ * @method SaleResponse sale(SaleRequest $request)
+ */
 interface VPosInterface
 {
 }
